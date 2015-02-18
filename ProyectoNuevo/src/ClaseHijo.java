6ec5b3302e1c15bdff3e0aa258faa6c0384aa05d
Demon_000 <Demon_000@192.168.1.200>
2015-02-18 12:31:55 +0100

    He creado de nuevo todo

---
+++ ProyectoNuevo/src/ClaseHijo.java
@@ -0,0 +1,12 @@
+public class ClaseHijo extends ClasePadre {
+	private String notas;
+
+	public ClaseHijo(int edad, String nombre) {
+		super(edad, nombre);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNotas() {
+		return notas;
+	}
+}
